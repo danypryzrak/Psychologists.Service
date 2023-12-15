e0350ed2875893a49f9c37c2b10dff83032f28ad
danypryzrak <116654935+danypryzrak@users.noreply.github.com>
2023-12-15 16:54:11 +0100

    sirst steps

---
+++ src/index.js
@@ -0,0 +1,6 @@
+console.log("hello")
+
+const p = document.createElement('p')
+
+const body = document.querySelector("body")
+
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      chunks: ['index'],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/styles', to: 'styles' }, // Копируем папку со стилями в dist
+      ],
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
