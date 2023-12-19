0d28b731fe7ec8e8c378852b58a5dbe3c4a06abd
Serhii Kazakevych <116654935+danypryzrak@users.noreply.github.com>
2023-12-19 11:24:27 +0100

    Delete src directory

--- src/js/getStart.js
+++
@@ -1,9 +0,0 @@
-const startBtn = document.querySelector(".hero__btn")
-
-
-
-if (document.location.pathname === '/index.html') {
-    startBtn.addEventListener('click', (ev) => {
-        window.location.href = './psychologists.html';
-    })
-}
